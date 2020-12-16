ed88c2ac64c8cb465a7a2df37e1d9499697f7ad7
felixbaumgartner99 <baumga92@gw.uni-passau.de>
2020-12-16 14:09:36 +0100

    Raumdaten ändern Formular integriert

--- UserSeiten/Betreiber_Admin/Raumverwaltung/raumdaten_ändern.php
+++ UserSeiten/Betreiber_Admin/Raumverwaltung/raumdaten_ändern.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Raumdaten ändern</title>
+    <link rel="stylesheet" type="text/css" href="Raumformularstylesheet.css" media="screen" />
 </head>
 
 <body>
+<br>
+<br>
 
 <?php
 
@@ -22,14 +25,113 @@ if($conn->connect_error){
         . $conn->connect_error);
 }
 
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+//Error_occured Variable (zunächst false)
+    $error = "";
+    $error_occured = false;
+
+//Abspeichern der zu ändernden Daten (man muss was eingeben? -> wäre einfacher)
+    $R_ID = $_POST["Raum-ID"];
+    $bezeichnung = $_POST["Raumbezeichnung"];
+    $kapazitaet = $_POST["RaumKapazität"];
+    $groesse = $_POST["RaumGröße"];
+    $preis = number_format((float)$_POST["Preis"], 2, '.', '');
+    $status = $_POST["Status"];
+
+//Abfrage, ob Raum ID des zu ändernden Raums existiert
+    $check_query = "SELECT R_ID FROM Raum WHERE R_ID = $R_ID";
+    $res1 = $conn->query($check_query);
+
+    if ($res1->num_rows == 0) {
+        $error = "Fehler: Angegebene Raum_ID existiert nicht";
+        $error_occured = true;
+    }
+
+//Abfrage, ob durch die Änderung der Kapazität des Raumes Veranstaltungen (auch bearbeitete Angebote?) existieren,
+//deren Teilnehmerzahl dann zu groß geworden ist ->trotzdem ändern lassen?? lt. Pflichtenheft ja aber das ist kompliziert
+    $check_query2 = "SELECT V_ID, Titel, Veranstalter, Teilnehmer_max, Beginn FROM Veranstaltung WHERE Ort = $R_ID AND Status = 1 AND Teilnehmer_max > $kapazitaet
+                     UNION
+                     SELECT BeAr_ID, 'Angebot bearbeitet', Veranstalter, Teilnehmer_gepl, Beginn FROM Anfrage_Angebot WHERE R_ID = $R_ID AND Status = 2 AND Teilnehmer_gepl > $kapazitaet";
+    $res2 = $conn->query($check_query2);
+    $show_table = false;
+
+    if($res2->num_rows > 0){
+        $error = "Es existieren Veranstaltungen und Angebote, die durch die Änderung der Kapazität betroffen sind!";
+        $error_occured = true;
+        $show_table = true;
+    }
+
+
+//Abfrage um bereits existierende Bezeichnung zu finden
+    $check_query = "SELECT R_ID FROM Raum WHERE R_ID != $R_ID AND Bezeichnung = '$bezeichnung' ";
+    $res3 = $conn->query($check_query);
+
+    if ($res3->num_rows > 0) {
+        $error =  $error . "<br>" . "Fehler: Raumbezeichnung existiert bereits.";
+        $error_occured = true;
+    }
+
+//Sonderzeichen Check
+    $check_sonderzeichen = preg_match('/^[-a-zA-ZÄÖÜäöüß0-9[:space:]]+$/', $bezeichnung);
+
+    if ($check_sonderzeichen == 0) {
+        $error = $error . "<br>" . "Fehler: Es dürfen nur Buchstaben, Zahlen und Bindestriche eingegeben werden!";
+        $error_occured = true;
+    }
+
+
+//Update Befehl und Überprüfung (nur wenn kein Fehler), ob es funktioniert hat (evtl Ausgabe)
+    if($error_occured == false) {
+
+
+        $update_query = "UPDATE Raum SET Bezeichnung = '$bezeichnung', Kapazitaet = $kapazitaet, Groesse = $groesse, Preis = $preis, Status = $status
+                     WHERE R_ID = $R_ID";
+
+        if ($conn->query($update_query) === TRUE) {
+            echo "<br>" . "Die Raumdaten wurden erfolgreich geändert.";
+
+        } else {
+            $error = "Es ist ein Fehler beim Einfügen in die Datenbank aufgetreten.";
+            $conn->error;
+            echo "<br>";
+        }
+    }
+
+}
+
+echo "<br><br>";
+
 //Ausgabe der existierenden Räume in einer Tabelle
 $räume_query = "SELECT R_ID, Bezeichnung, Kapazitaet, Groesse, Preis, Status FROM Raum";
 $result = $conn->query($räume_query);
 
 if($result->num_rows >0){
-    echo "<table border=\"1\">";
+echo "<table border=\"1\">";
     echo "<th>R_ID</th><th>Bezeichnung</th><th>Kapazität</th><th>Größe</th><th>Preis</th><th>Status</th>";
     while($i = $result->fetch_row()){
+    echo "<tr>";
+        foreach ($i as $item){
+        echo "<td>$item</td>";
+        }
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
+}
+
+echo "<br><br>";
+
+//Fehlermeldung wird ausgegeben
+echo $error;
+
+echo "<br><br>";
+
+
+//Anzeige der Tabelle der betroffenen Veranstaltungen
+if($show_table){
+    echo "<table border=\"1\">";
+    echo "<th>V_ID / Angebot_ID</th><th>Titel</th><th>Veranstalter ID</th><th>Max. Teilnehmerzahl</th><th>Beginn</th>";
+    while($i = $res2->fetch_row()){
         echo "<tr>";
         foreach ($i as $item){
             echo "<td>$item</td>";
@@ -38,107 +140,50 @@ if($result->num_rows >0){
     }
     echo "</table>\n";
 }
-
 ?>
 
 <br>
-<br>
-<form>
-    Hier kommt das Formular hin!!!
-</form>
+
+<div class="contact-us">
+    <h1> Raumdaten ändern</h1>
+    <!-- Fomular Spalten -->
+    <h3>
+        <em>&#x2a; </em> Bitte alle Felder ausfüllen um die Raumdaten zu ändern!
+    </h3>
+    <!--    <label style="position: relative" > Verpflichtend </label>-->
+
+    <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+        <label for="Raum-ID">Raum-ID <em>&#x2a;</em></label><input id="Raum-ID" name="Raum-ID" required="" type="Number"/>
+        <label for="Raumbezeichnung">Raumbezeichnung (nur Buchstaben / Leerzeichen und "-") <em>&#x2a;</em></label><input id="Raumbezeichnung" name="Raumbezeichnung" required="" type="text" maxlength="30"/>
+        <label for="RaumKapazität">Raum Kapazität <em>&#x2a;</em></label><input id="RaumKapazität" name="RaumKapazität" required="" type="Number" min="0" />
+        <label for="RaumGröße">Raumgröße in Quadratmetern</label><input id="RaumGröße" name="RaumGröße" pattern="[0-9][0-9][0-9]" type="Number"  min="0" />
+        <label for="Preis">Preis in Euro<em>&#x2a;</em></label><input id="Preis" name="Preis" required="" type="Number"  min="1" max="100000000"/>
+        <fieldset id = "Status">
+            <label for = "Status"> Raumstatus</label>
+            <input type= "radio" id="aktiv" name="Status" value="1">
+            <label for="aktiv"> aktiv</label>
+            <input type="radio" id="inaktiv" name="Status" value="2">
+            <label for="inaktiv"> inaktiv</label>
+        </fieldset>
+        <!--    <label for="Raumstatus">Raumstatus<em>&#x2a;</em></label><input id="Raumstatus" name="Raumstatus" required="" type="Number"  />-->
+        <!--    <form action="select.html">-->
+        <!--    <label>Raumstatus:-->
+        <!--        <select name="Status" size="2">-->
+        <!--            <option>aktiv</option>-->
+        <!--            <option>inaktiv</option>-->
+        <!--        </select>-->
+        <!--    </label>-->
+        <!--    </form>-->
 
 
 
-<?php
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-
-//Error Variable (zunächst false)
-    $error = false;
-
-//Abspeichern der zu ändernden Daten (man muss was eingeben? -> wäre einfacher)
-    $R_ID = $_POST[""];
-    $bezeichnung = $_POST[""];
-    $kapazitaet = $_POST[""];
-    $groesse = $_POST[""];
-    $preis = number_format((float)$_POST["Preis"], 2, '.', '');
-    $status = $_POST[""];
-
-//Abfrage, ob Raum ID des zu ändernden Raums existiert
-    $check_query = "SELECT R_ID FROM Raum WHERE R_ID = $R_ID";
-    $res1 = $conn->query($check_query);
-
-    if ($res1->num_rows == 0) {
-        echo "Fehler: Angegebene Raum_ID existiert nicht";
-        echo "<br>";
-        $error = true;
-    }
-
-//Abfrage, ob durch die Änderung der Kapazität des Raumes Veranstaltungen (auch bearbeitete Angebote?) existieren,
-//deren Teilnehmerzahl dann zu groß geworden ist ->trotzdem ändern lassen?? lt. Pflichtenheft ja aber das ist kompliziert
-    $check_query2 = "SELECT V_ID, Titel, Veranstalter, Teilnehmer_max, Beginn FROM Veranstaltung WHERE Status = 1 AND Teilnehmer_max > $kapazitaet
-                     UNION
-                     SELECT BeAr_ID, 'Angebot bearbeitet', Veranstalter, Teilnehmer_gepl, Beginn FROM Anfrage_Angebot WHERE Status = 2 AND Teilnehmer_gepl > $kapazitaet";
-    $res2 = $conn->query($check_query2);
-
-    if($res2->num_rows > 0){
-        echo "Es existieren folgende Veranstaltungen und Angebote, die durch die Änderung der Kapazität betroffen sind:";
-        echo "<br>";
-
-        echo "<table border=\"1\">";
-        echo "<th>V_ID / Angebot_ID</th><th>Titel</th><th>Veranstalter ID</th><th>Max. Teilnehmerzahl</th><th>Beginn</th>";
-        while($i = $res2->fetch_row()){
-            echo "<tr>";
-            foreach ($i as $item){
-                echo "<td>$item</td>";
-            }
-            echo "</tr>\n";
-        }
-        echo "</table>\n";
-    }
+        <button id="Ändern">Ändern</button>
+            <a href="#" type="button" class="Abbrechen">Abrechen</a>
 
 
-//Abfrage um bereits existierende Bezeichnung zu finden
-    $check_query = "SELECT R_ID FROM Raum WHERE Bezeichnung = '$bezeichnung'";
-    $res3 = $conn->query($check_query);
+    </form>
 
-    if ($res3->num_rows > 0) {
-        echo "Fehler: Raumbezeichnung existiert bereits.";
-        echo "<br>";
-        $error = true;
-    }
-
-//Sonderzeichen Check
-    $check_sonderzeichen = preg_match('/^[-a-zA-ZÄÖÜäöüß0-9[:space:]]+$/', $bezeichnung);
-
-    if ($check_sonderzeichen == 0) {
-        echo "Fehler: Es dürfen nur Buchstaben, Zahlen und Bindestriche eingegeben werden!";
-        echo "<br>";
-        $error = true;
-    }
-
-
-//Update Befehl und Überprüfung (nur wenn kein Fehler), ob es funktioniert hat (evtl Ausgabe)
-    if($error == false) {
-
-
-        $update_query = "UPDATE Raum SET Bezeichnung = '$bezeichnung', Kapazitaet = $kapazitaet, Groesse = $groesse, Preis = $preis, Status = $status
-                     WHERE R_ID = $R_ID";
-
-        if ($conn->query($update_query) === TRUE) {
-            echo "Die Raumdaten wurden erfolgreich geändert.";
-
-        } else {
-            echo "Es ist ein Fehler beim Einfügen in die Datenbank aufgetreten.";
-            $conn->error;
-            $error = true;
-            echo "<br>";
-        }
-    }
-
-}
-
-?>
+</div>
 
 </body>
 </html>
