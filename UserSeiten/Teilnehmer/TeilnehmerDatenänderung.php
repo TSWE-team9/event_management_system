389ee7f26d8aad54fe129b55b430532f99350ca3
chris-lupino <50741346+chris-lupino@users.noreply.github.com>
2020-12-18 11:17:47 +0100

    anfrage

--- UserSeiten/Teilnehmer/TeilnehmerDatenänderung.php
+++ UserSeiten/Teilnehmer/TeilnehmerDatenänderung.php
@@ -17,7 +17,7 @@
         <li><a href="#">Hilfe</a></li>
         <li><a href="TeilnehmerVeranstaltungen.php">Meine Veranstaltungen</a></li>
         <li style="text-align: right;"> <a href="../logout.php"> <i class="fas fa-sign-out-alt"></i> </a></li>
-        <li > <a class="active" href="TeilnehmerDatenänderung.php"> <i class="fas fa-user-circle"></i> </a></li>
+        <li> <a class="active" href="TeilnehmerDatenänderung.php"> <i class="fas fa-user-circle"></i> </a></li>
 
     </ul>
 </nav>
--- UserSeiten/Veranstalter/VeranstalterAnfrage.php
+++ UserSeiten/Veranstalter/VeranstalterAnfrage.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../CSS/Startseite.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="./anfrage.css">
     <title>Title</title>
 
     <script src="https://kit.fontawesome.com/23ad5628f9.js" crossorigin="anonymous"></script>
@@ -15,13 +16,56 @@
         <li><a href="#">Kontakt</a></li>
         <li><a href="#">Hilfe</a></li>
         <li><a href="VeranstalterVeranstaltungen.php">Meine Veranstaltungen</a></li>
-        <li style="text-align: right;"> <a href="../logout.php"> <i class="fas fa-sign-out-alt"></i> </a></li>
-        <li > <a href="VeranstalterDatenänderung.php"> <i class="fas fa-user-circle"></i> </a></li>
+        <li style="float: right;"> <a href="../logout.php"> <i class="fas fa-sign-out-alt"></i> </a></li>
+        <li style="float: right;"> <a href="VeranstalterDatenänderung.php"> <i class="fas fa-user-circle"></i> </a></li>
 
     </ul>
 </nav>
 
-<p>Anfrage</p>
+<h1>Anfrageformular für Veranstaltung</h1>
+<p>Beschreibung Formular</p>
+
+<!-- Anfrageformular -->
+<div class="container">
+    <form action="#" method="post">
+        <div class="row">
+            <div class="col-25">
+                <label for="teilnehmerzahl">Teilnehmerzahl</label>
+            </div>
+            <div class="col-75">
+                <input type="text" placeholder="geplante Teilnehmerzahl" name="teilnehmerzahl" pattern="[0-9]{1,50}" required>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="beginn">Veranstaltungsbeginn</label>
+            </div>
+            <div class="col-75">
+                <input type="date" name="date" required>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="dauer">Veranstaltungsdauer</label>
+            </div>
+            <div class="col-75">
+                <input type="number" name="dauer" min="1" max="7" required>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="anmerkungen">Anmerkungen</label>
+            </div>
+            <div class="col-75">
+                <textarea name="subject" id="subject" placeholder="Anmerkungen" cols="30" rows="10" maxlength="300"></textarea>
+            </div>
+        </div>
+        <div class="row">
+            <button class="btnanfrage" type="submit" name="anfrage">Anfrage abschicken</button>
+        </div>
+    </form>
+</div>
+
 <!--<footer>
    <div>
        <a href="#">Impressum</a>
