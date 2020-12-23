8738646851fb688892c70bd6d2670b1b543842f9
felixbaumgartner99 <baumga92@gw.uni-passau.de>
2020-12-23 11:53:08 +0100

    Raumreservierungsformular funktioniert

--- UserSeiten/Betreiber_Admin/Raumreservierung.php
+++ UserSeiten/Betreiber_Admin/Raumreservierung.php
@@ -7,6 +7,8 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <title>Raumreservierung</title>
+    <link rel="stylesheet" type="text/css" href="Raumformularstylesheet.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="header.css" media="screen" />
 </head>
 
 <body>
@@ -28,18 +30,15 @@ if($conn->connect_error){
 //Variablen
 $status = false;
 $query_status = "";
-$error = "";
 
 //Abspeichern der ID des ausgewählten Raumes und weiteren Variablen
-if(isset($_SERVER["Name des Buttons"])){
+if(isset($_POST["Reservieren"])){
 
-    $R_ID = $_POST["Name des Felds?"];
+    $R_ID = $_POST["Auswahl"];
     //Session für übergebene R_ID für Angebotserstellung und Veranstaltungserstellung (intern)
     $_SESSION["R_ID"] = $R_ID;
     $status = true;
 
-} else{
-    $error = "Es ist ein Fehler beim Empfang der Daten aus dem Formular aufgetreten";
 }
 
 //Reservierungs/Belegungsvorgang nur wenn auch eine R_ID empfangen wurde
@@ -47,7 +46,6 @@ if($status){
 
     //Reservierung eines Raumes für eine Anfrage
     if($_SESSION["Prüfungsart"] == 1){
-        echo "Reservierung";
 
         //Abspeichern der nötigen Daten für den INSERT in den Kalender
         $Beginn = $_SESSION["Beginn_final"];
@@ -76,7 +74,6 @@ if($status){
 
     //Belegung eines Raumes bei der Erstellung einer internen Veranstaltung
     elseif($_SESSION["Prüfungsart"] == 2){
-        echo "Belegung";
 
         //Abspeichern der nötigen Daten für den INSERT in den Kalender
         $Beginn = $_SESSION["Beginn_final"];
@@ -103,12 +100,79 @@ if($status){
     }
 }
 
-//Ausgabe möglicher Errors
-echo $error;
 ?>
 
 </body>
 
-<!---Hier kommt das Formular hin, in dem ausgewählt wird, welcher der verfügbaren Räume reserviert bzw. belegt werden soll--->
+<div class="contact-us">
+    <h1> Raum Reservieren</h1>
+    <!-- Fomular Spalten -->
+    <h3>
+        <em>&#x2a; </em> Bitte den gewünschten Raum aus der Liste auswählen.
+    </h3>
+
+    <div class="select-wrapper">
+        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+            <select class="auswahl" name="Auswahl">
+                <?php for($i = 0; $i<count($_SESSION["R_ID_Array"]); $i++){?>
+                <option value="<?php echo $_SESSION["R_ID_Array"][$i];?>"><?php echo "Raum " . $_SESSION["R_ID_Array"][$i];?> </option>
+                <?php }?>
+            </select>
+        <button type="submit"  class="Löschen" name="Reservieren"  value="Auswahl">Reservieren</button>
+        <a href="#" type="button" class="Abbrechen">Abbrechen</a>
+        </form>
+    </div>
+
+
+
+</div>
 
 </html>
+
+<style>
+    .auswahl {
+        width: 100%;
+        height: 40px;
+        /*-moz-padding-start: calc(10px - 3px);*/
+        padding-left: 10px;
+        background: white;
+        color: black;
+        font-family: 'Open Sans', sans-serif;
+        font-size: 16px;
+        box-shadow: 2px 2px 5px 1px rgba(0,0,0,0.3);
+        border-radius: 3px
+        outline: none;
+        cursor: pointer;
+        position: relative;
+        /*appearance: none;*/
+        /*display: grid;*/
+    }
+    .select-wrapper {
+        position: relative;
+        width:20em;
+        margin-bottom: 6em; }
+
+    }
+    .Löschen {
+        display:block;
+        float: right;
+        line-height: 24pt;
+        padding: 0 20px;
+        border: none;
+        background: #f45702;
+        color: white;
+        letter-spacing: 2px;
+        transition: 0.2s all ease-in-out;
+        border-bottom: 2px solid transparent;
+        outline: none;
+        margin-top: 30px;
+        font-size: 14px;
+
+    }
+    .Löschen:hover {
+        background: inherit;
+        color: #f45702;
+        border-bottom: 2px solid #f45702;
+        cursor: pointer;
+    }.
+</style>
