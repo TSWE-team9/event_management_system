ca94442cf40c0057e4b64f8ce89d134eaa42d8ba
SvenLau112 <svenja.awik@t-online.de>
2020-12-27 23:44:54 +0100

    Einfügen der Header

--- UserSeiten/Betreiber_Admin/KapazitätenabfrageV1.php
+++ UserSeiten/Betreiber_Admin/KapazitätenabfrageV1.php
@@ -6,14 +6,16 @@
     <title>Kapazitätenabfrage für Anfrage</title>
     <link rel="stylesheet" type="text/css" href="Kapazitätenstylesheet.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="header.css" media="screen" />
+    <script src="https://kit.fontawesome.com/23ad5628f9.js" crossorigin="anonymous"></script>
 </head>
 <body>
+
 <nav>
     <ul class="header">
         <li class="headerel"><a  href="StartseiteBetreiber.html" class ="headerel">Startseite</a></li>
-        <li class="headerel"><a href="#">Angebotserstellung</a></li>
+        <li class="headerel"><a class= "active" href="#">Angebotserstellung</a></li>
         <li class="headerel"><a href="#">Abrechnung</a></li>
-        <li class="headerel"><a class= "active" href="Raumverwaltung.php">Raumverwaltung</a></li>
+        <li class="headerel"><a  href="Raumverwaltung.php">Raumverwaltung</a></li>
         <li class="headerel"><a href="#">Meine Veranstaltungen</a></li>
         <li class="headerel"><a href="#">Statistiken</a></li>
         <li class="headerel" style="float: right;"> <a href="#"> <i class="fas fa-sign-out-alt"></i> </a></li>
@@ -21,6 +23,7 @@
 
     </ul>
 </nav>
+
 <div class="contact-us">
     <h1> Kapazitätenüberprüfung</h1>
     <!-- Fomular  mit Abfrage der gewünschten ID um freie Kapazitäten zu überprüfen-->
--- UserSeiten/Betreiber_Admin/KapazitätenabfrageV3.php
+++ UserSeiten/Betreiber_Admin/KapazitätenabfrageV3.php
@@ -5,14 +5,15 @@
     <title>Kapazitätenabfrage intern</title>
     <link rel="stylesheet" type="text/css" href="Kapazitätenstylesheet.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="header.css" media="screen" />
+    <script src="https://kit.fontawesome.com/23ad5628f9.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <nav>
     <ul class="header">
         <li class="headerel"><a  href="StartseiteBetreiber.html" class ="headerel">Startseite</a></li>
-        <li class="headerel"><a href="#">Angebotserstellung</a></li>
+        <li class="headerel"><a  class= "active" href="#">Angebotserstellung</a></li>
         <li class="headerel"><a href="#">Abrechnung</a></li>
-        <li class="headerel"><a class= "active" href="Raumverwaltung.php">Raumverwaltung</a></li>
+        <li class="headerel"><a href="Raumverwaltung.php">Raumverwaltung</a></li>
         <li class="headerel"><a href="#">Meine Veranstaltungen</a></li>
         <li class="headerel"><a href="#">Statistiken</a></li>
         <li class="headerel" style="float: right;"> <a href="#"> <i class="fas fa-sign-out-alt"></i> </a></li>
