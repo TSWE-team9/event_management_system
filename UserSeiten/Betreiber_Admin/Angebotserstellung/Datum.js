e25fcce16fa8cb269b97ecb21d4e7db3a00b8c4d
SvenLau112 <svenja.awik@t-online.de>
2021-01-03 11:23:13 +0100

    Datumsfunktion

---
+++ UserSeiten/Betreiber_Admin/Angebotserstellung/Datum.js
@@ -0,0 +1,77 @@
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
+
+// formats date to html format
+function dateToHtml(date) {
+    var year = date.getFullYear();
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+
+    if (month < 10) {
+        month = [0, month].join("");
+    }
+
+    if (day < 10) {
+        day = [0, day].join("");
+    }
+
+    var result = [year, month, day].join("-");
+    return result;
+}
+
+function dayOfWeek(date) {
+    var day = new Date(date).getDay();
+    var result;
+
+    switch(day) {
+        // sunday
+        case 0:
+            result = 1;
+            break;
+
+        // monday
+        case 1:
+            result = 7;
+            break;
+
+        // tuesday
+        case 2:
+            result = 6;
+            break;
+
+        // wednesday
+        case 3:
+            result = 5;
+            break;
+
+        // thursday
+        case 4:
+            result = 4;
+            break;
+
+        // friday
+        case 5:
+            result = 3;
+            break;
+
+        // saturday
+        case 6:
+            result = 2;
+            break;
+    }
+
+    return result;
+}
+
+function setDays() {
+    var maxDays = dayOfWeek(document.getElementById("Startdatum").value);
+    var maxDaysHtml = document.getElementById("Dauer").max = maxDays;
+}
+
+var currentDate = new Date();
+var minDate = dateToHtml(addDays(currentDate, 28));
+var initialDate = document.getElementById("Startdatum").value = minDate;
+var minDateHtml = document.getElementById("Startdatum").min = minDate;
