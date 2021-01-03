727e94b22846a2261b2c30d494847be76b115c1b
felixbaumgartner99 <baumga92@gw.uni-passau.de>
2021-01-03 14:02:06 +0100

    email test

--- test_directory/email_test.php
+++ test_directory/email_test.php
@@ -2,7 +2,8 @@
 
 $empfaenger = "felixbaumgartner13@gmail.com";
 $betreff = "Test";
-$from = "From: Felix Baumgartner <felixbaumgartner25@gmail.com>";
-$text = "Das ist ein Email Test";
+$from = "From: VMS Grup9 <vms.grup9@gmail.com>";
+$text = "Das ist ein Email Test. Es hat funktioniert.";
 
 mail($empfaenger, $betreff, $text, $from);
+echo "Die Email wurde versendet";
