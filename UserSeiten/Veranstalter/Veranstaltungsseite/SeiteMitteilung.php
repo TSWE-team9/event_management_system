3077505513299e688242505a3d7e89d79a86b0b0
felixbaumgartner99 <baumga92@gw.uni-passau.de>
2021-01-04 17:23:16 +0100

    Mitteilung versenden fertig (Button im Frontend auf der V Seite fehlt noch)

--- UserSeiten/Veranstalter/Veranstaltungsseite/SeiteMitteilung.php
+++ UserSeiten/Veranstalter/Veranstaltungsseite/SeiteMitteilung.php
@@ -1,3 +1,64 @@
+<?php
+session_start();
+include "../../send_email.php";
+
+//Verbindung zur Datenbank herstellen
+$host = '132.231.36.109';
+$db = 'vms_db';
+$user = 'dbuser';
+$pw = 'dbuser123';
+$conn = new mysqli($host, $user, $pw, $db,3306);
+
+
+//Überprüfen ob es einen Verbindungsfehler gab
+if($conn->connect_error){
+    die('Connect Error (' . $conn->connect_errno . ') '
+        . $conn->connect_error);
+}
+
+//Abspeichern der V_ID nach Buttonklick von der Veranstaltungsseite
+if(isset($_POST["mitteilung"])){
+    $_SESSION["V_ID"] = $_POST["veranstaltung_id"];
+}
+
+$V_ID = $_SESSION["V_ID"];
+
+//Abfragen des Titels der Veranstaltung
+$query = "SELECT Titel FROM Veranstaltung WHERE V_ID = $V_ID";
+$res = $conn->prepare($query);
+$res->execute();
+$res->bind_result($titel);
+$res->fetch();
+$res->close();
+
+//Abspeichern der Daten aus dem Mitteilungsformular, Versenden der Mitteilung
+if(isset($_POST["mitteilung_senden"])){
+    $V_ID = $_POST["v_id"];
+    $nachricht = $_POST["nachricht"];
+
+
+    //Angemeldete Teilnehmer per Mail benachrichtigen (nur offene Veranstaltungen)
+    $query = "SELECT B_ID FROM Teilnehmerliste_offen WHERE V_ID = $V_ID";
+    $res = $conn->query($query);
+    if($res->num_rows > 0){
+
+        $betreff = "Mitteilung vom Veranstalter";
+
+        while($i = $res->fetch_row()){
+            $empfaenger = get_mail_address($i[0]);
+            send_email($empfaenger, $betreff, $nachricht);
+        }
+    }
+
+    //Fehlermeldung ausgeben
+    else {
+        echo "Fehler: Es wurden keine Nachrichten verschickt (Keine Teilnehmer bzw. Fehler bei der Abfrage";
+    }
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,14 +85,14 @@
 </nav>
 
 <div class="container-50-outer">
-    <h1 class="hdln">#Veranstaltungstitel#</h1><!--TODO-->
+    <h1 class="hdln"><?php echo $titel; ?></h1>
     <p class="txt">Beschreibung</p>
 
-    <form action="#" method="post">
-        <textarea name="nachricht" placeholder="schreiben Sie hier den Inhalt iher Mitteilung" cols="30" rows="10" maxlength="300" required></textarea>
+    <form action="SeiteMitteilung.php" method="post">
+        <textarea name="nachricht" placeholder="Schreiben Sie hier den Inhalt ihrer Mitteilung" cols="30" rows="10" maxlength="300" required></textarea>
         
         <!--Button um die Mitteilungsversendung zu bestätigen-->
-        <button class="btn" style="float: right;" type="button" onclick="document.getElementById('id01').style.display='block'">Mitteilung versenden</button>
+        <button class="btn" style="float: right;" type="button" name ="mitteilung_senden" onclick="document.getElementById('id01').style.display='block'">Mitteilung versenden</button>
         <!--Modal wenn Veranstalter auf Ablehnen klickt-->
         <div id="id01" class="modal">
             <div class="modal_content">
@@ -39,7 +100,7 @@
                     <h1>Mitteilung versenden</h1>
                     <p>Wollen Sie diese Mitteilung an alle Teilnemer der Veranstaltung versenden?</p>
                     <div class="modal_clearfix">
-                        <input type="hidden" name="v_id" value="#v_id#"><!--TODO V_ID-->
+                        <input type="hidden" name="v_id" value="<?php echo $V_ID; ?>">
                         <button class="modal_btnconfirm" type="submit" name="mitteilung_senden" onclick="document.getElementById('id01').style.display='none'">Versenden</button>
                         <button class="modal_btnabort" type="button" onclick="document.getElementById('id01').style.display='none'">Abbrechen</button>
                     </div>
@@ -50,7 +111,7 @@
 
     <!--Button um zur Veranstaltungsseite zurückzukehren-->
     <form action="../../Veranstaltungsseite/VeranstaltungsSeite.php" method="post">
-        <input type="hidden" name="veranstaltung_id" value="#v_id#"><!--TODO V_ID-->
+        <input type="hidden" name="veranstaltung_id" value="<?php echo $V_ID; ?>">
         <button type="submit" class="btn" name="veranstaltung">zurück zur Veranstaltung</button> 
     </form>
 
