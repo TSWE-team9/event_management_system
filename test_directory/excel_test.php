558ef67251342af6bba1d0788a8a2b89a7f2d5dd
chris-lupino <50741346+chris-lupino@users.noreply.github.com>
2021-01-06 15:43:14 +0100

    excel test

---
+++ test_directory/excel_test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Excel test</title>
+    <script src="https://unpkg.com/read-excel-file@4.1.0/bundle/read-excel-file.min.js"></script>
+</head>
+<body>
+
+    <input type="file" id="input" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
+<script>
+    var input = document.getElementById("input");
+</script>
+</body>
+</html>
