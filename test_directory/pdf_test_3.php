46ee166b61a73a0b11a76a47e235117849ad163b
chris-lupino <50741346+chris-lupino@users.noreply.github.com>
2021-01-06 13:06:45 +0100

    pdf geht

--- test_directory/pdf_test_3.php
+++ test_directory/pdf_test_3.php
@@ -9,15 +9,54 @@
 
 <body>
 
+<?php
+    $phpArrayN = array(
+        0 => "Hans", 
+        1 => "Sepp", 
+        2 => "Franz",
+    )
+?>
+
+<?php
+$phpArrayV = array(
+        0 => "Maier", 
+        1 => "Huber", 
+        2 => "Weber", 
+    )
+?>
+
 <button type="button" onclick="genPDF()">Download</button>
-
-
 
 <script>
 
-    var dl = ["hello", "there"];
+    // getting arrays
+    var jArrayN = <?php echo json_encode($phpArrayN); ?>;
+    var jArrayV = <?php echo json_encode($phpArrayV); ?>;
 
-    var docDefinition = {dl};
+    var combinedArray = [];
+    for(var i = 0; i < jArrayN.length; i++) {
+        var temp = [jArrayN[i], jArrayV[i]].join(" ");
+        combinedArray.push(temp);
+    }
+
+    var titel = "Testveranstaltung";
+    var header = ["Teilnehmerliste für die Veranstaltung:", titel].join(" ");
+
+    var docDefinition = {
+        content:[
+            {text: header, style: 'header'},
+            " ", 
+            " ",
+            combinedArray
+        ],
+        styles: {
+            header: {
+                fontSize: 22,
+                bold: true,
+                alignment: 'center'
+            }
+        }
+    };
 
     function genPDF() {
         pdfMake.createPdf(docDefinition).download();
