20177483aac3bcf973f63e60c6b0e56f69f74bcf
chris-lupino <50741346+chris-lupino@users.noreply.github.com>
2021-01-07 11:04:46 +0100

    excel test 2

--- test_directory/excel_test_2.php
+++ test_directory/excel_test_2.php
@@ -13,6 +13,7 @@
     <p id="03">row3</p>
     <p id="04">row4</p>
     <button onclick="display()">click me</button>
+    <p id="error"></p>
 <script>
     var input = document.getElementById("input");
     var arr;
@@ -29,7 +30,34 @@
         document.getElementById("02").innerHTML = arr[1];
         document.getElementById("03").innerHTML = arr[2];
         document.getElementById("04").innerHTML = arr[3];
+
+        document.getElementById("error").innerHTML = phpN;
     }
+
+    var arrayN;
+    var arrayV;
+    // split 2d arrray into two seperate arrays
+    for(var i = 0; i < arr.length; i++) {
+        arrayN.push(i[0]);
+        arrayV.push(i[1]);
+    }
+
+
+    var phpN = arrayN.join(",");
+    var phpV = arrayV.join(",");
+    /*
+    var jsonN = JSON.stringify(arrayN);
+    var jsonV = JSON.stringify(arrayV);
+    // kann in php mit $array=json_decode($_POST['jsondata']);
+    /*
+    var len = arrayN.length();
+    for(var i = 0; i < arrayN.length, i++) {
+        len = i;
+        if(arrayN[i] == null) {
+            break;
+        }
+    }
+    */
     
 </script>
 </body>
