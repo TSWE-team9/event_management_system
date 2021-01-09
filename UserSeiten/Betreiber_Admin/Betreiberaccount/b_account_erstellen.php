eeab33014765cf3ab439cf6240a332d30d8251a5
SvenLau112 <svenja.awik@t-online.de>
2021-01-09 19:07:43 +0100

    Formular Betreiber

--- UserSeiten/Betreiber_Admin/Betreiberaccount/b_account_erstellen.php
+++ UserSeiten/Betreiber_Admin/Betreiberaccount/b_account_erstellen.php
@@ -17,7 +17,7 @@ if($conn->connect_error){
 $status = "";
 $error = false;
 
-if(isset($_POST["Name des Buttons"])){
+if(isset($_POST["erstellen"])){
 
     //Abspeichern der angegebenen Daten
     $email = $_POST["email"];
@@ -61,7 +61,13 @@ if(isset($_POST["Name des Buttons"])){
         }
     }
 
-    //TODO Ausgabe der Statusmeldung
+    echo "<div class='overlay'>" ;
+    echo  "<div class='popup'>";
+    echo "<h2>Meldung</h2>" ;
+    echo "<a class='close' href='b_account_erstellen.php'>&times;</a>" ;
+    echo "<div class='content'>".$status."</div>";
+    echo "</div>" ;
+    echo "</div>" ;
 
 }
 ?>
@@ -71,10 +77,61 @@ if(isset($_POST["Name des Buttons"])){
 <head>
     <title>Landing Page</title>
     <meta charset="utf-8"/>
+    <title>BetreiberAccount</title>
+
+    <link rel="stylesheet" type="text/css" href="../Raumverwaltung/Raumverwaltung.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../style/Fehlermeldung.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../Raumverwaltung/Raumformularstylesheet.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../style/header.css" media="screen" />
+    <!--    Einbinden von icons-->
+    <script src="https://kit.fontawesome.com/23ad5628f9.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
-<!--TODO Formular mit Email, Geburtsdatum (+check), Passwort und Auswahl der Rolle (Admin (4) oder Betreiber (3))-->
+<nav>
+    <ul class="header">
+        <li class="headerel"><a href="../Raumverwaltung/StartseiteBetreiber.html" class ="headerel">Startseite</a></li>
+        <li class="headerel"><a  href="../Angebotserstellung/Angebotserstellung.php">Angebotserstellung</a></li>
+        <li class="headerel"><a href="../Abrechnung/AbrechnungsSeite.php">Abrechnung</a></li>
+        <li class="headerel"><a  href="../Raumverwaltung/Raumverwaltung.php">Raumverwaltung</a></li>
+        <li class="headerel"><a href="../Angebotserstellung/InterneVeranstaltungen.php">Meine Veranstaltungen</a></li>
+        <li class="headerel"><a href="#">Statistiken</a></li>
+        <li class="headerel" style="float: right;"> <a href="#"> <i class="fas fa-sign-out-alt"></i> </a></li>
+        <li class="headerel" style=float:right;"> <a class= "active" href="#"  > <i class="fas fa-user-circle" ></i> </a></li>
+
+    </ul>
+</nav>
+<div class="contact-us" style="margin-top: 5em">
+    <h1> Account erstellen</h1>
+    <!-- Fomular Spalten -->
+    <h3>
+        <em>&#x2a; </em> Bitte alle Felder ausfüllen um einen neuen Account anzulegen.
+    </h3>
+    <form action="" method="post">
+        <label for="email">E-Mail-Adresse</label>
+        <input type="email" id="email" placeholder="E-Mail-Adresse" name="email" maxlength="50" required>
+
+        <label for="passwort">Passwort</label>
+        <input type="password" id="passwort" placeholder="Passwort" name="passwort_1" pattern=".{10,50}" required>
+
+        <label for="passwort">Passwort wiederholen</label>
+        <input type="password"  id="passwort" placeholder="Passwort" name="passwort_2" pattern=".{10,50}" required>
+
+        <label for="geb_t">Geburtsdatum</label>
+        <input type="date" name="geburtsdatum" id="geb_t" required>
+
+        <fieldset id = "Rolle">
+            <label for = "Rolle"> Rolle</label>
+            <input type= "radio" id="Betreiber" name="Rolle" value="3">
+            <label for="Betreiber"> Betreiber</label>
+            <input type="radio" id="Admin" name="Rolle" value="4">
+            <label for="Admin"> Administrator</label>
+        </fieldset>
+        <button class="Löschen" type="submit" name="erstellen">Erstellen</button>
+        <a href="../Raumverwaltung/StartseiteBetreiber.html" type="button" class="Abbrechen">Abrechen</a>
+    </form>
+</div>
+<script src="../../../LandingPage/index.js"></script>
 
 </body>
 
