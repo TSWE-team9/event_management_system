ded66faf7c4a287556a086bb791c0e4940628c07
SvenLau112 <svenja.awik@t-online.de>
2021-01-12 16:03:16 +0100

    Header

--- UserSeiten/Betreiber_Admin/Header/header.php
+++ UserSeiten/Betreiber_Admin/Header/header.php
@@ -7,7 +7,7 @@
         <li class="headerel"><a class="headerel" id="Reiter_Raumverwaltung" href="../Raumverwaltung/Raumverwaltung.php">Raumverwaltung</a></li>
         <li class="headerel"><a class="headerel" id="Reiter_MeineVeranstaltungen" href="../Angebotserstellung/InterneVeranstaltungen.php">Meine Veranstaltungen</a></li>
         <li class="headerel"><a class="headerel"id="Reiter_Statistiken" href="#">Statistiken</a></li>
-       <l class="headerel" style="float: right;"> <a id="Reiter_Logout" href="../../logout.php"> <i class="fas fa-sign-out-alt"></i> </a></li>
+       <li class="headerel" style="float: right;"> <a id="Reiter_Logout" href="../../logout.php"> <i class="fas fa-sign-out-alt"></i> </a></li>
      
     </ul>
 </nav>';
--- UserSeiten/logout.php
+++ UserSeiten/logout.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../UserSeiten/Betreiber_Admin/style/Fehlermeldung.css" media="screen" />
 </head>
 
 <body>
@@ -16,11 +17,20 @@ session_unset();
 session_destroy();
 
 $status = "Sie wurden erfolgreich ausgeloggt.";
-echo $status;
+echo "<div class='overlay'>" ;
+echo  "<div class='popup'>";
+echo "<h2>Info</h2>" ;
+echo "<a class='close' href='../LandingPage/index.php'>&times;</a>" ;
+echo "<div class='content' >" .$status."</div>";
+echo "</div>" ;
+echo "</div>";
 
 ?>
 
-<br>
-<a href='../LandingPage/index.php'>Zur Startseite</a>
 </body>
 </html>
+<style>
+    body{
+    background: url("../UserSeiten/Betreiber_Admin/style/safe-913452_1920.jpg") fixed;
+    }
+</style>
