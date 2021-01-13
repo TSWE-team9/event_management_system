9570d69f97c08273ee4cc02cff212e660cdb6e84
SvenLau112 <svenja.awik@t-online.de>
2021-01-13 09:39:34 +0100

    einbindung stylesheet

--- UserSeiten/Betreiber_Admin/Angebotserstellung/VeranstaltungBetreiber.php
+++ UserSeiten/Betreiber_Admin/Angebotserstellung/VeranstaltungBetreiber.php
@@ -6,6 +6,7 @@
     <title>Veranstaltung erstellen</title>
     <link rel="stylesheet" type="text/css" href="../style/Kapazitätenstylesheet.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="../style/header.css" media="screen" />
+    <link rel="stylsheet" type="text/css"   href="../style/Fehlermeldung.css"
     <script src="https://kit.fontawesome.com/23ad5628f9.js" crossorigin="anonymous"></script>
 </head>
 <body>
