75c64f73b72cf6d514da355eee6c70c651c07c4a
grup9vms <75378218+grup9vms@users.noreply.github.com>
2021-02-02 10:46:52 +0100

    delete testdirectory

--- test_directory/email_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$empfaenger = "felixbaumgartner13@gmail.com";
-$betreff = "Test";
-$from = "From: VMS Grup9 <vms.grup9@gmail.com>";
-$text = "Das ist ein Email Test. Es hat funktioniert.";
-
-mail($empfaenger, $betreff, $text, $from);
-echo "Die Email wurde versendet";
